04df62cedeabfd891a34f4d74fe5bfaf10a8dc95
Daniel <dualwinder777.2.0@gmail.com>
2025-04-02 14:21:46 -0300

    feat: add portfolio components and update configurations
    Introduce new components for the portfolio, including Header, Skill, and global styles. Update configurations for TailwindCSS, Netlify, and Astro. Add new assets like avatar.png and cuadricula.svg. Remove unused files and refine existing layouts and scripts for improved functionality and design.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
+import icon from 'astro-icon';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: netlify(),
+  integrations: [icon()],
+  output: 'server'
+});
